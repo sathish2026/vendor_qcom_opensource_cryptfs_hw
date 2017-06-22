92a91afd9c71e7fb1b629dc6ea0b200e4384768a
AnilKumar Chimata <anilc@codeaurora.org>
2017-06-22 23:55:07 +0530

    cryptfs_hw: Add new APIs for key management
    Add new APIs for create, wipe and update keys, which is
    required for HW based Full Disk Encryption.
    
    Change-Id: I483ce1a734db7b7cbfb2a06fe74baf559cfb51fb
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- Android.mk
+++ Android.mk
@@ -12,7 +12,8 @@ commonSharedLibraries := \
                         libhardware \
                         liblog
 commonIncludes := \
-                  hardware/libhardware/include/hardware/
+                  hardware/libhardware/include/hardware/ \
+                  $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
